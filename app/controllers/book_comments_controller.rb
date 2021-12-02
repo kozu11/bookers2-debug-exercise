3840c1fb0a82f64822e44271bc4e047ddd256d54
EC2 Default User <ec2-user@ip-172-31-17-93.ec2.internal>
2021-12-02 10:09:21 +0000

    [Update]いいね/コメント非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.book_id = @book.id
+    @book_comment.save
+   # binding.pry
+    render :comments
   end
   
   def destroy
-    BookComment.find_by(id: params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    render :comments
   end
   
   private
